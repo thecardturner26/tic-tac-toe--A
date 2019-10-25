3f90299258ccbfac813c499eb580cf198c74cbac
Marco Arteaga <marcoarteaga@Marcos-MacBook-Pro.local>
2019-10-25 16:29:13 -0500

    cleanUp code

--- js/main.js
+++ js/main.js
@@ -1,42 +1,65 @@
 /*----- constants -----*/
-let player1 = "X"
-let player2 = "O"
-
+let player1 = "X";
+let player2 = "O";
 
 
 /*----- app's state (variables) -----*/
-let X, O;
 
+let box = document.getElementsByClassName("block");
 
 /*----- cached element references -----*/
-//const blockEl = document.getElementsByClassName('block');
 
-
- const box = document.getElementsByClassName("block")
 
 /*----- event listeners -----*/
 
 
-box.addEventListener("click", handleBlockClick);
 
 
 
 /*----- functions -----*/
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+/*-------storage---------*/
 // function handleBlockClick(evt) {
 //     let X = evt.target.textContent;
-//         document.getElementsByClassName('block');
+//         //document.getElementsByClassName('block');
       
-//         console.log(evt)
-//         this.textContent = "X"
-//     return ;
+//         console.log(evt);
+//         //this.textContent = "X"
+//     //return ;
 // }
 
-function handleBlockClick() {
-    if (this.clasName = "played") {
-        this.textContent = 'X';
-        return;
+// function handleBlockClick() {
+//     if (this.clasName = "played") {
+//         this.textContent = 'X';
+//         return;
         
-    }
-}
-
+//     }
+// }
