e30d0bcde0acc51ff0876a369413b09fef2f074f
Marco Arteaga <marcoarteaga@Marcos-MacBook-Pro.local>
2019-10-25 15:07:22 -0500

    js update

--- js/main.js
+++ js/main.js
@@ -1,21 +1,35 @@
 /*----- constants -----*/
-
+const CHAC = ['X', 'O'];
 
 
 
 /*----- app's state (variables) -----*/
-
-
+let X, O;
 
 
 /*----- cached element references -----*/
+//const blockEl = document.getElementsByClassName('block');
 
 
-
+document.getElementsByClassName('block')
 
 /*----- event listeners -----*/
 
 
+    addEventListener('click', handleBlockClick);
+
 
 
 /*----- functions -----*/
+
+function handleBlockClick(evt) {
+    let X = evt.target.textContent;
+        document.getElementsByClassName('block');
+      
+        console.log(evt)
+        this.textContent = "X"
+    return ;
+
+
+}
+
